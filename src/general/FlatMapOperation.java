7b5d3fe108f7df789ef49a548732a0b7b4840643
kumarkunalbd <kumar.kunal.bd@gmail.com>
2025-01-05 11:41:44 +0530

    flatmap demo

---
+++ src/general/FlatMapOperation.java
@@ -0,0 +1,41 @@
+package general;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FlatMapOperation {
+
+    public static void main(String[] args) {
+        //flatmapdemo();
+        flatmapcollectionDemo();
+    }
+
+
+    public static void flatmapdemo(){
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(15);
+        list.add(23);
+        list.add(26);
+        list.add(19);
+        list.add(13);
+
+        list.stream().flatMap(num -> Stream.of(num + " plus")).forEach(str -> System.out.printf("entry is : %s\n",str));
+    }
+
+    public static void flatmapcollectionDemo() {
+        List<List<String>> list = new ArrayList<>();
+
+        list.add(Arrays.asList("Kunal","Rahul","Bunty"));
+        list.add(Arrays.asList("Kunal1","Rahul1","Bunty1"));
+        list.add(Arrays.asList("Kunal2","Rahul2","Bunty2"));
+        list.add(Arrays.asList("Kunal3","Rahul3","Bunty3"));
+        list.add(Arrays.asList("Kunal4","Rahul4","Bunty4"));
+
+        list.stream().flatMap(list1 -> list1.stream()).forEach(str -> System.out.printf("name is : %s\n",str));
+    }
+
+
+}
